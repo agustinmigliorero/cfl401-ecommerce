8a51036ef2944022c3809d753f0b5fee19d8ee72
Agustin Migliorero <agustinmigliorero@hotmail.com>
2023-11-03 20:02:56 -0300

    Creados las rutas de la aplicacion

---
+++ servidor/routes/comentario.js
@@ -0,0 +1,12 @@
+const routerComentarios = require("express").Router();
+const {
+  crearComentario,
+  editarComentario,
+  eliminarComentario,
+} = require("../controllers/comentario");
+
+routerComentarios.route("/").post(crearComentario);
+
+routerComentarios.route(":id").put(editarComentario).delete(eliminarComentario);
+
+module.exports = routerComentarios;
